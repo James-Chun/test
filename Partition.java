0ec4d34da0c0ec853d5dc2fe56fad90189fadb15
James Chun <jchun00@stuy.edu>
2019-03-08 10:19:05 -0500

    start

---
+++ Partition.java
@@ -0,0 +1,19 @@
+public class Partition{
+  /*Choose a random pivot element between the start and end index inclusive,
+ Then modify the array such that:
+ *1. Only the indices from start to end inclusive are considered in range
+ *2. A random index from start to end inclusive is chosen, the corresponding
+ *   element is designated the pivot element.
+ *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+ *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+ *@return the index of the final position of the pivot element.
+ */
+
+int partition ( int [] data, int start, int end){
+
+
+}
+
+
+
+}
