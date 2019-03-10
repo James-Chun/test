dbe505527247aa757b203f3de828f4e06c95b644
James-Chun <jchun00@stuy.edu>
2019-03-10 15:19:33 -0400

    made severe changes to how the sort works and tested using Victor Liu's Driver - works up to 2b

--- Partition.java
+++ Partition.java
@@ -15,17 +15,67 @@ private static Random rand = new Random();
 
 
 public static int partition ( int[] data, int start, int end){
-    //while (start != end){
-      int index = Math.abs(rand.nextInt() % data.length ) ; //making seed from that randgen
-    //}
 
-    return index;
+    int pivot = Math.abs(rand.nextInt() % data.length ) ; //making seed from that randgen
+
+    start++;
+    int temp = data[start-1];
+    data[start-1]=data[pivot];
+    data[pivot]=temp;
+    pivot = start -1;
+
+    while (start != end){
+      System.out.println(start);
+      System.out.println(end);
+      System.out.println(visual(data));
+
+            if ( data[start] > data[pivot] ){
+                temp = data[start];
+                data[start] = data[end];
+                data[end] = temp;
+                end--;
+            }
+
+            else if ( data[start] < data[pivot] ){
+              start++;
+            }
+
+    }
+
+    //System.out.println(start);
+    //System.out.println(end);
+    //System.out.println(visual(data));
+
+    if (data[start] > data[pivot]){
+        temp = data[start-1];
+        data[start-1]=data[pivot];
+        data[pivot]=temp;
+        System.out.println(visual(data));
+        return start-1;
+    }
+
+    temp = data[start];
+    data[start]=data[pivot];
+    data[pivot]=temp;
+    System.out.println(visual(data));
+
+    return start;
+}
+
+
+private static String visual(int[] data){
+  String visual = "[";
+    for (int v = 0; v < data.length; v++){
+        visual = visual + data[v];
+        if (v<data.length-1 ) visual += ", ";
+    }
+    return visual +"]";
 }
 
 
 public static void main(String[] args){
-  int[] data = {1, 2, 3, 4, 5,6,7,8,9,10,11};
-  System.out.println(partition(data,0,0));
+  //int[] data = {1, 2, 3, 4, 5,6,7,8,9,10,11};
+//  System.out.println(partition(data,0,0));
 }
 
 
